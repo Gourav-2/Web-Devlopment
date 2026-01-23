25253819680de7c60e3be8072e7bbef5a27562af
Gourav-2 <gc27112006@gmail.com>
2026-01-23 23:58:01 +0530

    code

---
+++ class12/index.js
@@ -0,0 +1,6 @@
+console.log("hello")
+let btn=document.querySelector("button")
+let body=document.querySelector("body")
+btn.addEventListener("click",function(){
+    body.style.backgroundColor='red'
+})
